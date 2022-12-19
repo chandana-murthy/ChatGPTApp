72911c1191d6c55804432f046054a05c49a4d974
Chandana Murthy <c.venkateshmurthy@stud.uni-goettingen.de>
2022-12-19 18:36:09 +0100

    Initial Commit

---
+++ ChatGPTApp/ChatGPTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTAppApp.swift
+//  ChatGPTApp
+//
+//  Created by Chandana Murthy on 19.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
